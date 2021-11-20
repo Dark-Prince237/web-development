d14da07a664f7e08aa5dee3c45232c166ba1015b
Dark-Prince237 <rashidiqbal7141@gmail.com>
2021-11-20 12:35:31 +0530

    hover tabs done

---
+++ Menus/Tabs/hover tabs/script.js
@@ -0,0 +1,46 @@
+let box=document.getElementsByClassName("box");
+// console.log(box);
+
+for(i=0;i<box.length;i++)
+{
+    box[i].addEventListener("mouseover",function()
+    {
+       
+        let prt=this.parentElement.parentElement.nextElementSibling;
+        // console.log(prt);
+        prt.style.display="block";
+     
+        // console.log(prt.firstElementChild.nextElementSibling.);       
+        if(this.innerHTML=="Patna")
+        {
+            prt.firstElementChild.innerHTML="Patna";
+            prt.firstElementChild.nextElementSibling.innerHTML="Patna is the capital city of Bihar.";
+
+        }
+        else if(this.innerHTML=="Banglore")
+        {
+            prt.firstElementChild.innerHTML="Banglore";
+            prt.firstElementChild.nextElementSibling.innerHTML="Banglore is the capital city of Karnataka.";
+
+        }
+
+        else
+        {
+            prt.firstElementChild.innerHTML="Amritsar"
+            prt.firstElementChild.nextElementSibling.innerHTML="Amritsar is the capital city  of Punjab.";
+
+        }
+    });
+
+    box[i].addEventListener("mouseout",function()
+    {
+        let prt=this.parentElement.parentElement.nextElementSibling;
+        prt.firstElementChild.innerHTML="";
+            prt.firstElementChild.nextElementSibling.innerHTML="";
+    });
+
+   
+
+}
+
+
